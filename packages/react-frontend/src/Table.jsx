9cff68fc8c80dd6d08278767b36e90e45a955331
Luke <luke.villaluz@gmail.com>
2024-09-27 14:39:50 -0700

    finished steps 5 and 6

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -16,10 +16,15 @@ function TableHeader() {
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
-      <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-      </tr>
+    <tr key={index}>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+        <button onClick={() => props.removeCharacter(index)}>
+          Delete
+        </button>
+      </td>
+    </tr>
     );
    }
   );
@@ -34,7 +39,10 @@ function Table(props) {
   return (
     <table>
       <TableHeader />
-      <TableBody characterData={props.characterData} />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
     </table>
   );
 }
