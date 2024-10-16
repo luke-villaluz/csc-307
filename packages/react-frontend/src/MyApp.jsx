43067e1654738e820b539a655f48b27659586d6a
luke-villaluz <luke.villaluz@gmail.com>
2024-10-15 21:16:24 -0700

    finished w step 1

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,9 +1,23 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+  
   function updateList(person) {
     setCharacters([...characters, person]);
   }
